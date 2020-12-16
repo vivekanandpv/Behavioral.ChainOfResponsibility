c59acba1913b606a12df08b2e147a17de76f844b
Vivekanand P V <vivekanandpv@outlook.com>
2020-12-16 12:22:40 +0530

    Add project files.

---
+++ Lib.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Behavioral.ChainOfResponsibility
+{
+    //  scalable conditional processing logic
+    //  if-else in an object-oriented way
+
+    public class LogMessage
+    {
+        public string Message { get; set; }
+    }
+
+
+
+    public interface ILogMessageHandler<T>
+    {
+        ILogMessageHandler<T> SetNext(ILogMessageHandler<T> nextHandler);
+        void Handle(T message);
+    }
+
+    public abstract class LogMessageHandler<T> : ILogMessageHandler<T> where T : class
+    {
+        private ILogMessageHandler<T> Next { get; set; }
+
+        public ILogMessageHandler<T> SetNext(ILogMessageHandler<T> nextHandler)
+        {
+            Next = nextHandler;
+            return Next;
+        }
+
+        public virtual void Handle(T message)
+        {
+            Next?.Handle(message);
+        }
+    }
+
+    //  Concrete handlers
+    public class ConsoleLogHandler : LogMessageHandler<LogMessage>
+    {
+        private readonly int _id;
+
+        public ConsoleLogHandler(int id)
+        {
+            _id = id;
+        }
+        public override void Handle(LogMessage message)
+        {
+            Console.WriteLine($"Console Logger {_id}: {message.Message}");
+            //  Separate validator class objects can be used
+            //  If the operation is not supported, they can throw the exception
+            base.Handle(message);
+        }
+    }
+
+    public class DebugLogHandler : LogMessageHandler<LogMessage>
+    {
+        private readonly int _id;
+
+        public DebugLogHandler(int id)
+        {
+            _id = id;
+        }
+        public override void Handle(LogMessage message)
+        {
+            Console.WriteLine($"Debug Logger {_id}: {message.Message}");
+            base.Handle(message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Behavioral.ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new ConsoleLogHandler(1);
+            logger.SetNext(new DebugLogHandler(1))
+                .SetNext(new ConsoleLogHandler(2))
+                .SetNext(new DebugLogHandler(2));
+            
+            logger.Handle(new LogMessage {Message = "Here is the message to log"});
+        }
+    }
+}
